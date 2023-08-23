34ed096441c6f6725992e2a92e27b4f53cefc085
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-08-23 10:21:54 +0400

    Add validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@ class User < ApplicationRecord
   attribute :bio, :text
   attribute :post_counter, :integer, default: 0
 
+  # Validations
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
   # Methods
   def three_most_recent_posts
     posts.order(created_at: :desc).limit(3)
