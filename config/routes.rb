29cc9af8adf9de8ce23138b3237ed1d2774606a9
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-09-06 15:01:18 +0400

    Refactor root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
+  root to 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
