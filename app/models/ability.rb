e9e8389aebcbc16062a6894d753cc17996a63aaf
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-09-07 18:52:25 +0400

    Refactor Ability class for user role-based permissions

---
+++ app/models/ability.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # Guest user
+
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :read, :all
+      can :delete, [Post, Comment], author_id: user.id
+    end
+  end
+end
